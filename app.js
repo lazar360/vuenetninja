cfc6a83818f41f11654db3440a86ee7adee7ce88
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-05-04 17:18:42 +0200

    add click event

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const app = Vue.createApp({
-
+    data() {
+        return {
+           title : 'The Final Empire',
+           author : 'George R. R. Martin',
+           age : 45,
+        }
+    }
 })
 
 app.mount('#app')
